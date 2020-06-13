afc6d01da6e5f51d6a627c13dc8a3bfcee08866d
Felipe D S Lima <felipe@monts.com.br>
2020-06-13 17:37:07 -0300

    rename the app

--- android/app/src/main/java/br/com/monts/bino/MainActivity.java
+++ android/app/src/main/java/br/com/monts/bino/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "BinoAssistenteCCR";
+    return "Bino";
   }
 
   @Override
