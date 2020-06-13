20f85176cce6277a0c976efc29139778acb7b37d
Felipe D S Lima <felipe@monts.com.br>
2020-06-13 03:22:41 -0300

    add routes to app

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import StackNavigator from './StackNavigator';
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
