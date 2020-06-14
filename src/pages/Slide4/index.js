b7dae9a36ed132fadf4fa5a34f7914dbc2b91479
Felipe D S Lima <felipe@monts.com.br>
2020-06-14 00:23:39 -0300

    Slide 4

--- src/pages/Slide4/index.js
+++ src/pages/Slide4/index.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import { View } from 'react-native';
+import PropTypes from 'prop-types';
+
+import Background from '~/components/Background';
+import binoImg from '~/assets/images/bino-head.png';
+import check from '~/assets/images/check.png';
+import arrowLeft from '~/assets/images/arrow-backward.png';
+import {
+  Image,
+  LeftText,
+  BoldText,
+  ArrowRight,
+  ArrowLeft,
+  ButtonLink,
+} from './styles';
 
 // import { Container } from './styles';
 
-const Slide4 = () => {
-  return <View />;
+const Slide4 = ({ navigation }) => {
+  function next() {
+    // navigation.navigate('Slide4');
+  }
+
+  function prev() {
+    navigation.navigate('Slide3');
+  }
+
+  return (
+    <Background>
+      <LeftText firstLine>Tente falar</LeftText>
+      <LeftText>
+        <BoldText>“Ei Bina”</BoldText> agora!
+      </LeftText>
+      <ButtonLink onPress={next}>
+        <ArrowRight style={{ resizeMode: 'stretch' }} source={check} />
+      </ButtonLink>
+      <ButtonLink onPress={prev}>
+        <ArrowLeft style={{ resizeMode: 'stretch' }} source={arrowLeft} />
+      </ButtonLink>
+      <Image style={{ resizeMode: 'stretch' }} source={binoImg} />
+    </Background>
+  );
+};
+
+Slide4.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
 };
 
 export default Slide4;
---
+++ src/pages/Slide4/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+export const Image = styled.Image`
+  width: 144px;
+  height: 176px;
+  margin: 0px 16px 64px auto;
+`;
+
+export const ButtonLink = styled(TouchableOpacity)`
+  margin: 16px 0;
+  width: 100%;
+`;
+
+export const ArrowRight = styled.Image`
+  width: 80px;
+  height: 80px;
+  margin: 0 16px 0 auto;
+`;
+
+export const ArrowLeft = styled.Image`
+  width: 80px;
+  height: 80px;
+`;
+
+export const LeftText = styled.Text`
+  margin: ${props =>
+    props.firstLine ? '40px 40px 0 16px' : '0 40px 16px 16px'};
+  font-size: 40px;
+`;
+
+export const BoldText = styled.Text`
+  font-weight: bold;
+`;
