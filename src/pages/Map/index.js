306ede50ee1ea7e3bb2fa898951ea9cbe9d9b359
Felipe D S Lima <felipe@monts.com.br>
2020-06-14 22:44:20 -0300

    fix something

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -3,40 +3,39 @@ import MapView from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
 const Map = () => {
-  const [location, setLocation] = useState();
-  Geolocation.watchPosition(
-    info =>
-      setLocation({
-        ...info.coords,
-        latitudeDelta: 0.0042,
-        longitudeDelta: 0.0031,
-      }),
-    () => {},
-    { maximumAge: 1000, distanceFilter: 0.5 }
-  );
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.0042,
+    longitudeDelta: 0.0031,
+  });
 
   useEffect(() => {
-    console.log(location);
+    if (location.latitude === 0 && location.longitude === 0) {
+      Geolocation.getCurrentPosition(info =>
+        setLocation({
+          ...info.coords,
+          latitudeDelta: 0.0042,
+          longitudeDelta: 0.0031,
+        })
+      );
+    }
   }, [location]);
 
   return (
-    <>
-      {location && (
-        <MapView
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-          }}
-          initialRegion={location}
-          region={location}
-          onRegionChange={setLocation}
-          showsUserLocation
-        />
-      )}
-    </>
+    <MapView
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+      }}
+      initialRegion={location}
+      region={location}
+      onRegionChange={setLocation}
+      showsUserLocation
+    />
   );
 };
 
